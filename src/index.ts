231358fe8919be3c626d2b59ebae3cee4c0b1413
romantic <cesc_fabregas1414@hotmail.com>
2019-12-08 05:24:05 +0700

    master

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { ReferenceItem } from "./class";
+function hello(say: string = "world"): void {
+  console.log(`Hello ${say}.`);
+}
 
-// press f5 to build
-let ref: ReferenceItem = new ReferenceItem("Update Facts and Figures", 2019);
-ref.printItem();
-// invoked set publisher
-ref.publisher = "Random Data Publishing";
-// invoked get publisher
-console.log(ref.publisher);
+hello();
