439a8bd236b1de45e7e27af539a51d798ae12e01
romantic <cesc_fabregas1414@hotmail.com>
2019-12-08 05:14:50 +0700

    [feature/class]: demo creating and using classes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-/**
- * Create a promise that resolves after some time
- * @param n number of milliseconds before promise resolves
- */
-function timeout(n: number) {
-  return new Promise(res => setTimeout(res, n));
-}
+import { ReferenceItem } from "./class";
 
-/**
- * Add three numbers
- * @param a first number
- * @param b second
- */
-export async function addNumbers(a: number, b: number) {
-  await timeout(500);
-  return a + b;
-}
-
-//== Run the program ==//
-(async () => {
-  console.log(await addNumbers(3, 4));
-})();
+// press f5 to build
+let ref: ReferenceItem = new ReferenceItem("Update Facts and Figures", 2019);
+ref.printItem();
+// invoked set publisher
+ref.publisher = "Random Data Publishing";
+// invoked get publisher
+console.log(ref.publisher);
