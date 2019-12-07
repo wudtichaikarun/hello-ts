88c70344044d2992d4f7bbc74d8d76dd61c6f398
romantic <cesc_fabregas1414@hotmail.com>
2019-12-08 05:21:45 +0700

    [feature/class]: demo creating and using classes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { ReferenceItem } from "./class";
 
 // press f5 to build
+/**----------class----------  */
 let ref: ReferenceItem = new ReferenceItem("Update Facts and Figures", 2019);
 ref.printItem();
 // invoked set publisher
 ref.publisher = "Random Data Publishing";
 // invoked get publisher
 console.log(ref.publisher);
+
+/**----------subject----------  */
+/**----------subject----------  */
+/**----------subject----------  */
