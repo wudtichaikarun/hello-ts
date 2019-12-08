cecb852f2dd31cfd75cfe3393e0d73e823664a0d
romantic <cesc_fabregas1414@hotmail.com>
2019-12-09 02:52:04 +0700

    [feature/basicTypes]: variable declarations, Array, Tuples, Object, Interfaces, Intersection, Union

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,68 @@
-function hello(say: string = "world"): void {
-  console.log(`Hello ${say}.`);
-}
+import { HasEmail, HasPhoneNumber } from "./interface";
+/**
+ *  Variable declarations
+ */
+const a: string = "romantic";
+const b: number = 12;
 
-hello();
+/**
+ * Arrays & Tuples
+ */
+// array
+let n: number[] = [1, 2];
+// n.push("1"); // got error
+
+// tuples, which has a fixed length
+let nn: [number, string, boolean] = [100, "romantic", true];
+// Note tuples should not use Array method because it's not type safe
+nn.push(1, 2, 3, 4); // should error but not!!!
+
+/**
+ * Object types & Interfaces
+ */
+// Object
+let address: { houseNumber: number; streetName: string };
+address = {
+  houseNumber: 1234,
+  streetName: "AA Street"
+};
+// Object with optional operator (?)
+let addressB: { houseNumber: number; streetName?: string };
+addressB = {
+  houseNumber: 111
+};
+// Interfaces
+interface Address {
+  houseNumber: number;
+  streetName?: string;
+}
+const addressC: Address = {
+  houseNumber: 222
+};
+
+/**
+ * Intersection & Union types
+ * - The `and` and `or` operators
+ */
+// Intersection types `|`
+let contactInfo: HasEmail | HasPhoneNumber =
+  Math.random() > 0.5
+    ? {
+        // assign it to a HasPhoneNumber
+        name: "Romantic",
+        phone: 12345678
+      }
+    : {
+        // assign it to a HasEmail
+        name: "Romantic",
+        email: "ro@ro.com"
+      };
+// contactInfo. // Note: we can only access the .name property
+
+// Union types `&`
+let otherContactInfo: HasEmail & HasPhoneNumber = {
+  // we must initialize it to a shape that's assignable to HasEmail and HasPhoneNumber
+  name: "Mike",
+  email: "Mike@mail.com",
+  phone: 233445566
+};
---
+++ src/interface.ts
@@ -0,0 +1,9 @@
+export interface HasPhoneNumber {
+  name: string;
+  phone: number;
+}
+
+export interface HasEmail {
+  name: string;
+  email: string;
+}
