dd044da7558e99a7f7b05ca33c68368135535205
romantic <cesc_fabregas1414@hotmail.com>
2019-12-11 01:15:37 +0700

    [feature/basicTypes]: interfaces & type aliases

--- src/index.ts
+++ src/index.ts
@@ -117,3 +117,97 @@ contactPeople("phone", { name: "Ro A", phone: 1234567 });
  * Lexical scope
  * - Lexical scope just mean, what is the value of `this` when you invoke a function
  */
+
+/**
+ * Type Aliases & extends
+ */
+// Type aliases
+type StringOrNumber = string | number;
+const x: StringOrNumber = 2;
+// this i the ONLY time you'll see a type on the RHS of assignment
+type HasName = { name: string };
+
+// Extends
+// Interfaces can extend from other interfaces
+interface HasInternationalPhoneNumber extends HasPhoneNumber {
+  countryCode: string;
+}
+
+/**
+ * Call & Construct signatures
+ */
+// Construct (function signature)
+interface ContactMessenger1 {
+  (contact: HasEmail | HasPhoneNumber, message: string): void;
+}
+// same as above just syntax sugar
+type ContactMessenger2 = (
+  contact: HasEmail | HasPhoneNumber,
+  message: string
+) => void;
+// Note: we don't need type annotations for contact or message
+const emailer: ContactMessenger1 = (_contact, _message) => {
+  // implement logic..
+};
+// construct signatures can be described as well
+interface ContactConstructor {
+  new (...args: any[]): HasEmail | HasPhoneNumber;
+}
+
+/**
+ * Dictionary Objects & Index
+ */
+interface PhoneNumberDict {
+  // arr[o], foo['myProp']
+  [numberName: string]:
+    | undefined
+    | {
+        areaCode: number;
+        num: number;
+      };
+}
+
+const d: PhoneNumberDict = {};
+if (d.abc) {
+  d.abc;
+}
+
+const phoneDict: PhoneNumberDict = {
+  office: { areaCode: 321, num: 55555555 }
+  // home: { areaCodeeeeee: 333, num: 222222 } // try editing
+};
+
+/**
+ * Recursive type
+ */
+// problem data type for value 1 | 2 | 3 | array that contain value of 1 | 2 | 3
+// type NumVal = 1 | 2 | 3 | NumArr // got error because it's circle ref
+// type NumArr = NumVal[]
+
+// use hosting concept to fix the problem
+// link to explain how the hosting work  https://www.udemy.com/course/understand-javascript/learn/lecture/2237452#overview
+type NumVal = 1 | 2 | 3 | NumArr;
+interface NumArr extends Array<NumVal> {}
+const xx: NumVal = 1;
+const xxx: NumVal = [1, 2, 3, 1, 1, [2, 3, 1]];
+const xxxx: NumVal = [1, 2, 3];
+// const aa: NumVal = 4; // got error
+// const aaa: NumVal = [1,4]; // got error
+
+/** Lazy versus Eager
+ * What difference between `Type And Interface` ?
+ * - Type aliases are eager
+ * - Interfaces are lazy
+ */
+
+/**
+ * Type test module name `json-types`
+ * - link https://frontendmasters.com/courses/typescript-v2/type-tests/
+ */
+
+/**
+ * Classes  Access modifier keywords - `who can access this thing`
+ * - public - everyone
+ * - protected - me and subclasses
+ * - private - only me
+ */
